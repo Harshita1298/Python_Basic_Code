7199ceef7bdde5524bd05f65bad71779a71e1b3d
Harshita Gupta <148797967+Harshita1298@users.noreply.github.com>
2026-04-21 22:40:17 +0530

    Update and rename demo.py to lambda.py

---
+++ lambda.py
@@ -0,0 +1,10 @@
+
+# Add 10 to the argument
+add_ten = lambda a: a + 10
+print(add_ten(5)) # Output: 15
+# Multiply two arguments
+multiply = lambda a, b: a * b
+print(multiply(5, 6)) # Output: 30
+# Sum three arguments
+sum_three = lambda a, b, c: a + b + c
+print(sum_three(5, 6, 2)) # Output: 13
